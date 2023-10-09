e7a003a9d0ff3b28b1114e03a1f6dc3cf98272d9
Mwambui123 <maurinawambuia@gmail.com>
2023-10-09 13:56:39 +0300

    more comments

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-
+//The receivesAFunction function should:take a callback function as an argument, call the callback function. it doesn't matter what this function returns, so long as it calls the callback function
 function receivesAFunction (callback){
    callback()
 
 }
+
+//The returnsANamedFunction function should:take no arguments, return a named functio
 
 function returnsANamedFunction(){
     return function aNamedFunction(){
 
     }
 }
-
+//The returnsAnAnonymousFunction function should:take no arguments, return an anonymous function
 function returnsAnAnonymousFunction(){
     return function(){
 
